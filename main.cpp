4254460f12ba7992c5e8662b869820a3964ebd66
LucaUnige <118013855+LucaUnige@users.noreply.github.com>
2022-11-11 19:44:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(){
-float a=0
-float b=0
-float c=0
+float a=0;
+float b=0;
+float c=0;
 cin >> a >> b >> c;
-cout << ( a * b) / 2  << endl 
-cout << ( a * a) << endl
-cout << ( a * b) << endl
-cout << (a + b) * c << endl
+cout << ( a * b) / 2  << endl; 
+cout << ( a * a) << endl;
+cout << ( a * b) << endl;
+cout << (a + b) * c << endl;
 }
